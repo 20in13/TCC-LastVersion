2ad56ce205cedeab43f0f6d928afdf2b13d4597b
20in13 <128084778+20in13@users.noreply.github.com>
2024-05-24 11:37:18 -0300

    ajuste de leiaute

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,8 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <h1>Projeto</h1>
+    <div className='container'>
+      <h1>Projeto nextJS Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aspernatur at excepturi maxime, quod modi recusandae impedit nam enim debitis, dolores totam amet? Corporis esse culpa porro, eius ipsa quos voluptatibus soluta numquam, necessitatibus provident doloribus enim. Officia, itaque expedita ea sit nobis culpa tenetur error, magnam dolor earum iure quidem eligendi perspiciatis ullam obcaecati laborum deserunt ex voluptatem ipsam! Ratione doloribus, aut minus consequuntur sed ex? Doloribus, consequuntur quaerat.</h1>
+    </div>
   );
 }
