e1b164a24b9bc8c63196ec571543d422ffa96df1
20in13 <128084778+20in13@users.noreply.github.com>
2024-06-18 11:45:50 -0300

    18/06 v2

--- src/app/components/NavRail/navrail.jsx
+++ src/app/components/NavRail/navrail.jsx
@@ -1,43 +1,98 @@
-import styles from "./navrail.module.css";
+// components/Sidebar.js
+import Link from 'next/link';
+import { FaHome, FaStar, FaSearch, FaCalendarAlt, FaUserCircle } from 'react-icons/fa';
 
-const NavigationRail = () => {
+const Sidebar = () => {
   return (
+    <div className="sidebar">
+      <div className="logo">
+        <img src="/logo.png" alt="Logo" className="logo-img" />
+      </div>
+      <div className="menu">
+        <button className="menu-btn">
+          <FaCalendarAlt className="icon" />
+        </button>
+        <div className="menu-list">
+          <Link href="/">
+            <a className="menu-item">
+              <FaHome className="icon" />
+              <span>Home</span>
+            </a>
+          </Link>
+          <Link href="/favoritos">
+            <a className="menu-item">
+              <FaStar className="icon" />
+              <span>Favoritos</span>
+            </a>
+          </Link>
+          <Link href="/pesquisar">
+            <a className="menu-item">
+              <FaSearch className="icon" />
+              <span>Pesquisar</span>
+            </a>
+          </Link>
+        </div>
+      </div>
+      <div className="footer">
+        <FaUserCircle className="icon-footer" />
+      </div>
+      <style jsx>{`
+        .sidebar {
+          width: 80px;
+          height: 100vh;
+          background-color: #ff6961; /* fundo vermelho pastel */
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          padding: 20px 0;
+        }
+        .logo {
+          margin-bottom: 30px;
+        }
+        .logo-img {
+          width: 50px;
+          height: 50px;
+          border-radius: 50%;
+        }
+        .menu {
+          flex-grow: 1;
+        }
+        .menu-btn {
+          background: none;
+          border: none;
+          margin-bottom: 30px;
+          cursor: pointer;
+        }
+        .menu-list {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+        .menu-item {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          margin-bottom: 20px;
+          color: #fff;
+          text-decoration: none;
+        }
+        .icon {
+          font-size: 24px;
+          margin-bottom: 5px;
+        }
+        .menu-item span {
+          font-size: 12px;
+        }
+        .footer {
+          margin-top: auto;
+        }
+        .icon-footer {
+          font-size: 50px;
+          color: #fff;
+        }
+      `}</style>
+    </div>
+  );
+};
 
-<nav class="navbar">
-  <div class="logo">
-    <img src="logo.png" alt="Logo" class="rounded-circle" />
-  </div>
-  <div class="nav-btn">
-    <button class="btn btn-agenda">
-      <i class="fas fa-calendar-alt"></i>
-    </button>
-  </div>
-  <ul class="nav-list">
-    <li>
-      <button class="btn btn-home">
-        <i class="fas fa-home"></i>
-        <span>Home</span>
-      </button>
-    </li>
-    <li>
-      <button class="btn btn-favorites">
-        <i class="fas fa-heart"></i>
-        <span>Favoritos</span>
-      </button>
-    </li>
-    <li>
-      <button class="btn btn-search">
-        <i class="fas fa-search"></i>
-        <span>Pesquisar</span>
-      </button>
-    </li>
-  </ul>
-  <div class="profile-icon">
-    <i class="fas fa-user-circle" style="font-size: 40px;"></i>
-  </div>
-</nav>
-
-);
-}
-
-export default NavigationRail;
+export default Sidebar;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,25 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import NavigationRail from "./components/NavRail/navrail";
+import Sidebar from "./components/NavRail/navrail";
 
 export default function Home() {
-  return (
 
-    <NavigationRail> </NavigationRail>
+    return (
+      <div className="app-container">
+        <Sidebar />
+        <div className="main-content">
+          <Component {...pageProps} />
+        </div>
+        <style jsx global>{`
+          .app-container {
+            display: flex;
+          }
+          .main-content {
+            flex-grow: 1;
+          }
+        `}</style>
+      </div>
+    );
 
-  );
+
 }
