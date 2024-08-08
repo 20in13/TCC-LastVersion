81531c0037fd711bb2d2e792871a4165219cf271
20in13 <128084778+20in13@users.noreply.github.com>
2024-08-08 15:04:57 -0300

    08/08

---
+++ src/app/screens/Testes/page.jsx
@@ -0,0 +1,7 @@
+import '../styles/global.css'; // Certifique-se de que o caminho está correto
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
